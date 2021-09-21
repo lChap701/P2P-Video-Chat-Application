8b78d6d20b1fb62b49cc79504dfc685b2e8bb9ca
lChap701 <chapmanlucas201@gmail.com>
2021-09-20 20:28:45 -0500

    Added the ability to access video and audio

--- public/js/script.js
+++ public/js/script.js
@@ -9,7 +9,32 @@ window.onload = () => {
     } else {
       window.location.pathname += answer;
     }
+  } else {
+    accessVideo();
   }
 };
 
+function accessVideo() {
+  let myVideoStream;
+  const myVideo = document.createElement("video");
 
+  myVideo.muted = true;
+  navigator.mediaDevices
+    .getUserMedia({
+      audio: true,
+      video: true,
+    })
+    .then((stream) => {
+      myVideoStream = stream;
+      addVideoStream(myVideo, stream);
+    });
+}
+
+function addVideoStream(video, stream) {
+  video.srcObject = stream;
+  
+  video.addEventListener("loadedmetadata", () => {
+    video.play();
+    document.querySelector("#videos").append(video);
+  });
+}

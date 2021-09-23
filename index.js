439c6822603b6fc1aa60bf675c30fe4814069ee5
lChap701 <chapmanlucas201@gmail.com>
2021-09-22 23:26:17 -0500

    Version 2

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
+// Socket.io Setup
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
 // Allows stylesheets, JS scripts, and other files to be loaded
 app.use(express.static(__dirname + "/public"));
 
@@ -14,12 +25,83 @@ app.get("/", (req, res) => {
   res.render("index", { room: "" });
 });
 
+// Array of users connected to a room
+let connectedUsers = [];
+
 // Display index.ejs with the room that was entered
 app.get("/:room", (req, res) => {
+  io.on("connection", (socket) => {
+    socket.on("disconnect", () => {
+      connectedUsers = connectedUsers.filter(
+        (user) => user.id !== socket.id && user.room == req.params.room
+      );
+      socket.broadcast.emit("updateUserList", {
+        users: connectedUsers,
+      });
+
+      console.log(connectedUsers);
+    });
+
+    socket.on("joinRoom", (room, username) => {
+      // Checks if the user should join the room
+      if (connectedUsers.filter((user) => user.room == room).length < 2) {
+        socket.join(room);
+
+        let res =
+          connectedUsers.length > 0
+            ? connectedUsers.find((user) => user.id === socket.id)
+            : undefined;
+
+        if (res == undefined) {
+          connectedUsers.push({
+            id: socket.id,
+            username: username,
+            room: room,
+          });
+        }
+
+        console.log(connectedUsers);
+
+        let usersInRoom = connectedUsers.filter((user) => user.room == room);
+
+        socket.on("message", (message) => {
+          io.to(room).emit("createMessage", message, socket.id, username);
+        });
+
+        socket.on("requestUserList", () => {
+          socket.emit("updateUserList", { users: usersInRoom });
+          socket.broadcast.emit("updateUserList", { users: usersInRoom });
+        });
+
+        socket.on("mediaOffer", (data) => {
+          socket.to(data.to).emit("mediaOffer", {
+            from: data.from,
+            offer: data.offer,
+          });
+        });
+
+        socket.on("mediaAnswer", (data) => {
+          socket.to(data.to).emit("mediaAnswer", {
+            from: data.from,
+            answer: data.answer,
+          });
+        });
+
+        socket.on("iceCandidate", (data) => {
+          socket.to(data.to).emit("remotePeerIceCandidate", {
+            candidate: data.candidate,
+          });
+        });
+      } else {
+        socket.emit("roomFull");
+      }
+    });
+  });
+
   res.render("index", { room: req.params.room });
 });
 
 // Sets the port used to access my app
-const listener = app.listen(process.env.PORT || 8080, () => {
+const listener = server.listen(process.env.PORT || 8080, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
--- public/js/script.js
+++ public/js/script.js
@@ -1,37 +1,102 @@
+const socket = io("/");
+const peer = new RTCPeerConnection({
+  iceServers: [
+    {
+      urls: "stun:stun.stunprotocol.org",
+    },
+  ],
+});
+const message = document.querySelector("input[name='message']");
+const invite = document.querySelector("button#invite");
+const video = document.querySelector("button#video");
+const audio = document.querySelector("button#audio");
+const messages = document.querySelector("#messages");
+const remoteVideo = document.querySelector("#remoteVideo");
+let myVideoStream;
+let newUser;
+
+/* Attempts to send a message when the 'send' button is clicked */
+document.querySelector("button#send").addEventListener("click", () => {
+  if (message.value.trim().length !== 0) {
+    socket.emit("message", message.value.trim());
+    message.value = "";
+  }
+});
+
+/* Attempts to send a message when the 'Enter' key is pressed */
+message.addEventListener("keydown", (e) => {
+  if (e.key == "Enter" && message.value.trim().length !== 0) {
+    socket.emit("message", message.value.trim());
+    message.value = "";
+  }
+});
+
+/* Gives the user the link to use for invites */
+invite.addEventListener("click", () => {
+  prompt(
+    "Copy this link and send it to people you want to meet with:",
+    location.href
+  );
+});
+
+/* Disables or enables access to camera */
+video.addEventListener("click", () => {
+  const enabled = myVideoStream.getVideoTracks()[0].enabled;
+
+  if (enabled) {
+    myVideoStream.getVideoTracks()[0].enabled = false;
+    video.innerHTML = `<i class="fas fa-video-slash"></i>`;
+  } else {
+    myVideoStream.getVideoTracks()[0].enabled = true;
+    video.innerHTML = `<i class="fas fa-video"></i>`;
+  }
+
+  video.classList.toggle("bg-red");
+});
+
+/* Disables or enables access to microphone */
+audio.addEventListener("click", () => {
+  const enabled = myVideoStream.getAudioTracks()[0].enabled;
+
+  if (enabled) {
+    myVideoStream.getAudioTracks()[0].enabled = false;
+    audio.innerHTML = `<i class="fas fa-microphone-slash"></i>`;
+  } else {
+    myVideoStream.getAudioTracks()[0].enabled = true;
+    audio.innerHTML = `<i class="fas fa-microphone"></i>`;
+  }
+
+  audio.classList.toggle("bg-red");
+});
+
 /**
  * Gets the name of the room or prepares to access video and audio
  */
-window.onload = () => {
-  const roomEl = document.querySelector("#video-container h2");
+const init = () => {
+  if (document.querySelector("#video-container h2").innerText == "") {
+    let room = prompt("Please enter the name of this room", "Room-1");
 
-  if (roomEl.innerText == "") {
-    let room = window.prompt("Please enter the name of this room", "Room-1");
-
-    if (room == null) {
-      window.location.reload();
+    if (room == null || room.trim().length == 0) {
+      location.reload();
     } else {
-      window.location.pathname += room.replace(new RegExp(" ", "g"), "-");
+      location.pathname += room.replace(new RegExp(" ", "g"), "-");
     }
   } else {
-    let username = window.prompt("Please enter your username", "john-smith");
+    let username = prompt("Please enter your username", "john-smith");
 
     if (username == null) {
-      window.location.reload();
+      location.reload();
     } else {
-      console.log(username.replace(new RegExp(" ", "g"), "-"));
-      accessVideo();
+      accessVideo(username.replace(new RegExp(" ", "g"), "-"));
     }
   }
 };
 
 /**
  * Access the camera and microphone
+ * @param {string} username   Represents the username that was chosen
  */
-function accessVideo() {
-  let myVideoStream;
-  const myVideo = document.createElement("video");
-
-  myVideo.muted = true;
+function accessVideo(username) {
   navigator.mediaDevices
     .getUserMedia({
       audio: true,
@@ -39,20 +104,150 @@ function accessVideo() {
     })
     .then((stream) => {
       myVideoStream = stream;
-      addVideoStream(myVideo, stream);
-    });
+      document.querySelector("#localVideo").srcObject = stream;
+      stream.getTracks().forEach((track) => peer.addTrack(track, stream));
+
+      socket.emit("joinRoom", ROOM, username);
+      socket.emit("requestUserList");
+    })
+    .catch((err) => alert(err));
 }
+
+/*
+ * Displays prompts, accesses video, and gets the initial users
+ */
+socket.on("connect", init);
+
+/* Gets the new user */
+socket.on("updateUserList", async ({ users }) => {
+  console.log(users);
+
+  if (users.length == 2) {
+    invite.disabled = true;
+    newUser = users.filter((user) => user.id !== socket.id)[0];
+    await addUser();
+  } else {
+    invite.disabled = false;
+    remoteVideo.style.display = "none";
+  }
+});
 
 /**
- * Streams the video using the camera and microphone
- * @param {HTMLVideoElement} video  Represents the newly created video element
- * @param {MediaStream}     stream  Represents the video being streamed
+ * Adds a new user to the room
  */
-function addVideoStream(video, stream) {
-  video.srcObject = stream;
+const addUser = async () => {
+  console.log("addUser");
+  console.log(socket.id);
+  const localPeerOffer = await peer.createOffer();
+  await peer.setLocalDescription(localPeerOffer);
 
-  video.addEventListener("loadedmetadata", () => {
-    video.play();
-    document.querySelector("#videos").append(video);
+  socket.emit("mediaOffer", {
+    offer: localPeerOffer,
+    from: socket.id,
+    to: newUser.id,
   });
-}
+};
+
+/**
+ * Creates a call for adding a new user
+ * @param {*} data    Represents the data to use for adding this user
+ */
+const onMediaOffer = async (data) => {
+  try {
+    console.log("onMediaOffer");
+    console.log(socket.id);
+    console.log(data.offer);
+    await peer.setRemoteDescription(data.offer);
+    const peerAnswer = await peer.createAnswer();
+    await peer.setLocalDescription(peerAnswer);
+
+    socket.emit("mediaAnswer", {
+      answer: peerAnswer,
+      from: socket.id,
+      to: data.from,
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+socket.on("mediaOffer", onMediaOffer);
+
+/**
+ * Answers the call that was created
+ * @param {*} data  Represents the data that should be used to answer the call
+ */
+const onMediaAnswer = async (data) => {
+  console.log("onMediaAnswer");
+  console.log(socket.id);
+  await peer.setRemoteDescription(data.answer);
+};
+
+socket.on("mediaAnswer", onMediaAnswer);
+
+/**
+ * Creates the ICE candidate
+ * @param {RTCPeerConnectionIceEvent} e   Represents the event that occurred
+ */
+const onIceCandidateEvent = (e) => {
+  console.log("onIceCandidateEvent");
+
+  socket.emit("iceCandidate", {
+    to: newUser.id,
+    candidate: e.candidate,
+  });
+};
+
+peer.onicecandidate = onIceCandidateEvent;
+
+/**
+ * Transfers the ICE candidate to another peer
+ * @param {*} data    Represents the data to transfer
+ */
+const onRemotePeerIceCandidate = async (data) => {
+  try {
+    console.log("onRemotePeerIceCandidate");
+    await peer.addIceCandidate(new RTCIceCandidate(data.candidate));
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+socket.on("remotePeerIceCandidate", onRemotePeerIceCandidate);
+
+/**
+ * Adds the other stream to the peer
+ *
+ * @param {RTCTrackEvent} e   Represents the event that occurred
+ */
+const gotRemoteStream = (e) => {
+  console.log("gotRemoteStream");
+  const [stream] = e.streams;
+  remoteVideo.removeAttribute("style");
+  remoteVideo.srcObject = stream;
+};
+
+peer.addEventListener("track", gotRemoteStream);
+
+/* Adds messages that were recently sent */
+socket.on("createMessage", (message, userId, username) => {
+  // Setup message container
+  const messageEl = document.createElement("div");
+  messageEl.className = "message";
+
+  // Add user info to container
+  const bUserInfo = document.createElement("b");
+  bUserInfo.id = "user-info";
+  const icon = document.createElement("i");
+  icon.className = "far fa-user-circle";
+  bUserInfo.append(icon, userId == socket.id ? "me" : username);
+  messageEl.append(bUserInfo);
+
+  // Add message to container
+  const spanMessage = document.createElement("span");
+  spanMessage.append(message);
+  messageEl.append(spanMessage);
+
+  // Add message container to the document
+  messages.append(messageEl);
+});
